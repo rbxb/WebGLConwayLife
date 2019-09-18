dde70e057e4e3e0dd3526881ab21586878bccd70
ryan <26235793+rbxb@users.noreply.github.com>
2019-09-18 11:03:34 -0500

    resizable canvas & cell map

--- game2.js
+++ game2.js
@@ -25,7 +25,7 @@ function gol() {
 	const golFragmentShaderSource = `
 		precision mediump float;
 		uniform sampler2D tex;
-		uniform vec2 scale;
+		uniform float scale;
 		uniform vec3 color;
 	
 		int get(vec2 xy) {
@@ -56,7 +56,6 @@ function gol() {
 	const scaleInput = overlay.querySelector("#scale-input");
 	const frequencyInput = overlay.querySelector("#frequency-input");
 	const colorInputs = overlay.querySelectorAll(".color-input");
-	const golScale = {x:256,y:256};
 	var positionBuffer;
 
 	var golCameraPosition = {x:0.0,y:0.0};
@@ -72,6 +71,7 @@ function gol() {
 		frequency: 32,
 		colorScale: {r:0.2,g:0.86,b:0.94},
 	};
+	var mapScale;
 
 	function main() {
 		gl = canvas.getContext("webgl");
@@ -79,51 +79,39 @@ function gol() {
 			alert("WebGL not supported.");
 			return null;
 		}
-
-		canvas.width = canvas.clientWidth;
-		canvas.height = canvas.clientHeight;
 		
 		positionBuffer = createPositionBuffer();
 
 		var viewProgramInfo = createViewProgram();
 		var golProgramInfo = createGolProgram();
 
-		var surfaces = [];
-		var surfaceCount = 2;
-		for (var i = 0; i < surfaceCount; i++) {
-			const tex = createTexture(golScale);
-			const framebuffer = gl.createFramebuffer();
-			gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-			gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
-			gl.clearColor(0.0,0.0,0.0,1.0);
-			gl.clear(gl.COLOR_BUFFER_BIT);
-			surfaces.push({tex:tex,framebuffer:framebuffer,scale:golScale});
-		}
-		var currentSurface = 0;
+		var swapchain;
 
 		var lastTimestamp = 0;
 		var lastCalc = 0;
 		function draw(timestamp) {
+			canvas.width = canvas.clientWidth;
+			canvas.height = canvas.clientHeight;
+			if (mapScale != settings.mapScale) {
+				mapScale = settings.mapScale;
+				swapchain = new Swapchain(2,mapScale);
+			}
 			var dif = timestamp - lastTimestamp;
 			lastTimestamp = timestamp;
-			//msMeter.innerHTML = Math.round(dif * 10) / 10 + " ms";
 			if (play && timestamp - lastCalc > settings.frequency) {
 				lastCalc = timestamp;
 				gl.useProgram(golProgramInfo.program);
-				gl.bindTexture(gl.TEXTURE_2D, surfaces[currentSurface].tex);
-				currentSurface++;
-				if (currentSurface >= surfaceCount) {
-					currentSurface = 0;
-				}
-				gl.bindFramebuffer(gl.FRAMEBUFFER, surfaces[currentSurface].framebuffer);
+				gl.bindTexture(gl.TEXTURE_2D, swapchain.cur().tex);
+				gl.bindFramebuffer(gl.FRAMEBUFFER, swapchain.next().framebuffer);
 				gl.uniform3f(golProgramInfo.color,settings.colorScale.r,settings.colorScale.g,settings.colorScale.b);
-				gl.viewport(0, 0, golScale.x, golScale.y);
+				gl.uniform1f(golProgramInfo.scale,mapScale);
+				gl.viewport(0, 0, mapScale, mapScale);
 				gl.drawArrays(gl.TRIANGLES, 0, 6);
 			}
 			gl.useProgram(viewProgramInfo.program);
 			gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 			gl.viewport(0, 0, canvas.width, canvas.height);
-			gl.bindTexture(gl.TEXTURE_2D, surfaces[currentSurface].tex);
+			gl.bindTexture(gl.TEXTURE_2D, swapchain.cur().tex);
 			for (var i = 0; i < pokes.length; i++) {
 				p = pokes.pop();
 				var color = [0,0,0,0];
@@ -133,17 +121,42 @@ function gol() {
 				gl.texSubImage2D(gl.TEXTURE_2D, 0, p.x, p.y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(color));
 			}
 			if (randomMap != null) {
-				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, golScale.x, golScale.y, 0, gl.RGBA, gl.UNSIGNED_BYTE, randomMap);
+				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, mapScale, mapScale, 0, gl.RGBA, gl.UNSIGNED_BYTE, randomMap);
 				randomMap = null;
 			}
 			gl.clearColor(0.0,0.0,0.0,1.0);
 			gl.clear(gl.COLOR_BUFFER_BIT);
 			gl.uniform2f(viewProgramInfo.cam,golCameraPosition.x,golCameraPosition.y);
 			gl.uniform1f(viewProgramInfo.zoom,golZoom);
+			gl.uniform2f(viewProgramInfo.scale,canvas.width,canvas.height);
 			gl.drawArrays(gl.TRIANGLES, 0, 6);
 			window.requestAnimationFrame(draw);
 		}
 		window.requestAnimationFrame(draw);
+	}
+
+	function Swapchain(count,scale) {
+		var a = [];
+		var c = 0;
+		for (var i = 0; i < count; i++) {
+			const tex = createTexture(scale);
+			const framebuffer = gl.createFramebuffer();
+			gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+			gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, tex, 0);
+			gl.clearColor(0.0,0.0,0.0,1.0);
+			gl.clear(gl.COLOR_BUFFER_BIT);
+			a.push({tex:tex,framebuffer:framebuffer,scale:scale});
+		}
+		this.cur = function() {
+			return a[c];
+		}
+		this.next = function() {
+			c++;
+			if (c >= count) {
+				c = 0;
+			}
+			return a[c];
+		}
 	}
 
 	function createViewProgram() {
@@ -152,7 +165,6 @@ function gol() {
 		programInfo.cam = gl.getUniformLocation(programInfo.program, 'cam');
 		programInfo.zoom = gl.getUniformLocation(programInfo.program, 'zoom');
 		programInfo.scale = gl.getUniformLocation(programInfo.program, 'scale');
-		gl.uniform2f(programInfo.scale,canvas.width,canvas.height);
 		return programInfo;
 	}
 
@@ -161,7 +173,6 @@ function gol() {
 		bindPositionAttribute(programInfo,positionBuffer);
 		programInfo.scale = gl.getUniformLocation(programInfo.program, 'scale');
 		programInfo.color = gl.getUniformLocation(programInfo.program, 'color');
-		gl.uniform2f(programInfo.scale,golScale.x,golScale.y);
 		return programInfo;
 	}
 
@@ -222,7 +233,7 @@ function gol() {
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, scale.x, scale.y, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, scale, scale, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 		return tex;
 	}
 
@@ -298,8 +309,8 @@ function gol() {
 				play = !play;
 				break;
 			case 82:
-				randomMap = new Uint8Array(golScale.x * golScale.y * 4);
-				for (var i = 0; i < golScale.x * golScale.y; i++) {
+				randomMap = new Uint8Array(mapScale * mapScale * 4);
+				for (var i = 0; i < mapScale * mapScale; i++) {
 					var pos = i * 4;
 					var color;
 					if (Math.random() > 0.5) color = [255,255,255,255];
@@ -353,13 +364,12 @@ function gol() {
 
 	function poke(x,y,alive) {
 		var p = {
-			//(gl_FragCoord.xy / scale - 0.5) * zoom + cam
-			x: Math.round(((x / canvas.width - 0.5) * golZoom - golCameraPosition.x) * golScale.x * (canvas.width/canvas.height) - 0.5) % golScale.x,
-			y: Math.round(((1 - y / canvas.height - 0.5) * golZoom - golCameraPosition.y) * golScale.y - 0.5) % golScale.y,
+			x: Math.round(((x / canvas.width - 0.5) * golZoom - golCameraPosition.x) * mapScale * (canvas.width/canvas.height) - 0.5) % mapScale,
+			y: Math.round(((1 - y / canvas.height - 0.5) * golZoom - golCameraPosition.y) * mapScale - 0.5) % mapScale,
 			alive: alive,
 		};
-		if (p.x < 0) p.x = golScale.x + p.x;
-		if (p.y < 0) p.y = golScale.y + p.y;
+		if (p.x < 0) p.x = mapScale + p.x;
+		if (p.y < 0) p.y = mapScale + p.y;
 		pokes.push(p);
 	}
 
